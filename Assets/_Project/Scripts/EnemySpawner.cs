a3a89342631ee3c366f5e420e32654f34764d4c1
yugd1211 <yugd1211@gmail.com>
2025-01-05 19:26:58 +0900

    [STYLE] : 바닥, 에너미 에셋 전부 변경

--- Assets/_Project/Scripts/EnemySpawner.cs
+++ Assets/_Project/Scripts/EnemySpawner.cs
@@ -5,24 +5,23 @@ using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
-	public Enemy enemyPrefab;
+	public Enemy[] enemyPrefabs;
+	public Enemy bossPrefab;
 	public Transform[] spawnPoints;
 	public float spawnRate = 1f;
 	public int spawnCount = 10;
-
-	private void Update()
-	{
-		print(Random.Range(0, spawnPoints.Length));
-	}
 
 	private IEnumerator SpawnEnemy()
 	{
 		while (spawnCount-- > 0)
 		{
 			Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+			Enemy enemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
 			Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity).transform.SetParent(transform);
 			yield return new WaitForSeconds(spawnRate);
 		}
+		
+		Instantiate(bossPrefab, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity).transform.SetParent(transform);
 		
 		yield return new WaitForSeconds(5f);
 		if (GameManager.Instance.currentWave < GameManager.Instance.maxWave)
